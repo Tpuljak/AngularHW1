82beaadec5bcdbbaa85a9974dd20691f58fbc021
Toma Puljak <toma.puljak@hotmail.com>
2017-05-22 22:36:04 +0200

    Added files + basic functions

---
+++ scripts/app/app.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('App', [
+    'LocalStorageModule'
+]);
+
+app.run(function (localStorageService, $state) {
+    var students = [
+        {
+            name: "Toma Puljak",
+            gender: "M",
+            dateOfAddition: new Date(Math.random() * new Date())
+        }
+    ];
+    localStorageService.set('students', angular.toJson(students));
+});
+
+app.controller("FirstController", function ($scope, localStorageService, $state) {
+    $scope.addingStudent = false;
+    $scope.addNewStudent = function () {
+        $scope.addingStudent = !$scope.addingStudent;
+    };
+
+    $scope.confirm = function () {
+        $state.students.Add({
+            name: element(by.model('name')),
+            gender: element(by.model('gender')),
+            dateofAddition: new Date(Math.random() * new Date())
+        });
+        $scope.addingStudent = false;
+    };
+});
